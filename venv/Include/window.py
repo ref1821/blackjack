8d1f31d881e9113f21ffc793ccd457635f937960
ref1821 <ref1821@gmail.com>
2021-01-10 22:04:12 -0600

    version casi estable (ya no tanto)

--- venv/Include/window.py
+++ venv/Include/window.py
@@ -12,7 +12,9 @@ def exit():
     root.destroy()
 
 
-first =str(randint(1,13))
+first = randint(1,13)
+second = randint(1, 13)
+
 
 def but1():
     exit()
@@ -22,37 +24,61 @@ def but1():
     root2.config(bg = "black")
     root2.geometry("500x600")
 
+    def another_card():
+
+        final = first + second
+        third = randint(1, 13)
+        while final <= 21:
+            if final <= 21:
+                final = final + third
+                final_Label = Label(root2, text = "my new number is" + str(final))
+                final_Label.grid()
+            else:
+                end_label = Label(root2, text = "you lost")
+                end_label.grid()
+
+
+
     frame = LabelFrame(root2, padx = 30, pady = 30, bg = "grey")
     frame.grid(row = 0, column = 0)
     frame2 = LabelFrame(root2, padx=30, pady=30, bg="grey")
     frame2.grid(row = 0, column = 1)
 
-    new_card = Button(frame, text="NEW CARD", command=but1, bg="green", padx = 50)
+    new_card = Button(frame, text="NEW CARD", command=another_card, bg="green", padx = 50)
     new_card.grid()
     stop_card = Button(frame2, text = "I don't want other" ,command =but1, bg = "red", padx = 50)
     stop_card.grid()
 
-    my_number = Label(root2, text = "my number is " + first, fg = "green", bg =  "black")
+    my_number = Label(root2, text = "my number is " + str(first) + " and " + str(second), fg = "green", bg =  "black")
     my_number.grid(column = 0, row = 1,)
+
+
+
+
+
 
     root2.mainloop()
 
 
 def but2():
     root3 = Tk()
-
+    root2.title("2 player")
+    root2.config(bg="black")
+    root2.geometry("500x600")
 
     root3.mainloop()
 
 def but3():
     root4 = Tk()
-
+    root2.title("3 player")
+    root2.config(bg="black")
+    root2.geometry("500x600")
 
     root4.mainloop()
 labelj= Label(root, text = "how many players?", fg = "dark green", bg = "black")
 j1 = Button(root, text="1 player ", command = but1, padx = 2, bg = "green")
-j2 = Button(root, text="2 players", command = but1, bg = "green")
-j3 = Button(root, text="3 players", command = but1, bg = "green")
+j2 = Button(root, text="2 players", command = but2, bg = "green")
+j3 = Button(root, text="3 players", command = but3, bg = "green")
 
 
 
